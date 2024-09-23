d7ffc0f4fe57da2b5d472da95b62f81a99694445
Owen Chiu <owencc8@gmail.com>
2024-09-23 06:07:56 -0400

    home pg done

---
+++ script.js
@@ -0,0 +1,18 @@
+//return to top btn https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
